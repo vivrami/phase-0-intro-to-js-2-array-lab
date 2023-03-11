dcb52394a4a167bcfafe31b999d449e85e566cf0
vivianna ramirez <vivrami2001@gmail.com>
2023-03-10 22:45:17 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+cats.push("Ralph")
+};
+function destructivelyPrependCat(name) {
+cats.unshift("Bob")
+};
+function destructivelyRemoveLastCat(name) {
+cats.pop()
+}
+function destructivelyRemoveFirstCat(name) {
+cats.shift()
+}
+function appendCat(name) {
+    return [...cats, name];
+}
+function prependCat(name) {
+    return [name, ...cats]
+}
+function removeLastCat(name) {
+    const copyCats = cats.slice()
+    return copyCats.slice(0, copyCats.length - 1) ;
+}function removeFirstCat(name) {
+    const copyCats = cats.slice()
+    return cats.slice(1)
+}
